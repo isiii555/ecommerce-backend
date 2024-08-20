9e4811e540f059d9f9f19d77d564eca67bda8d8e
İslam Salamzadə <islam.salamzade.191@gmail.com>
2024-08-20 17:02:11 +0400

    cluster added

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const cluster = require('cluster');
+const os = require('os');
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
@@ -11,23 +13,39 @@ const app = express();
 const port = process.env.SERVER_PORT;
 const con_str = process.env.CON_STR;
 
-mongoose
-  .connect(con_str)
-  .then(() => {
-    console.log("Server connected to mongodb");
-  })
-  .catch((err) => {
-    console.log("Server could not connect to mongodb");
+if (cluster.isMaster) {
+  const numCPUs = os.cpus().length;
+
+  console.log(`Master ${process.pid} is running`);
+  console.log(`Forking ${numCPUs} workers...`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Forking a new one...`);
+    cluster.fork();
   });
+} else {
+  mongoose
+    .connect(con_str)
+    .then(() => {
+      console.log("Server connected to mongodb");
+    })
+    .catch((err) => {
+      console.log("Server could not connect to mongodb");
+    });
 
-app.use(express.json());
+  app.use(express.json());
 
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/products", productRoutes);
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/orders", orderRoutes);
-app.use("/api/v1/basket", basketRoutes);
+  app.use("/api/v1/users", userRoutes);
+  app.use("/api/v1/products", productRoutes);
+  app.use("/api/v1/auth", authRoutes);
+  app.use("/api/v1/orders", orderRoutes);
+  app.use("/api/v1/basket", basketRoutes);
 
-app.listen(port, () => {
-  console.log(`Server started listening @ port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Server started listening @ port ${port}`);
+  });
+}
